35e6a695fcc58e53bc24b3b5e37ff3eca67743c3
Paulie Pena <paulie@zolabooks.com>
2013-10-10 13:01:57 -0400

    created test-runner.py

--- ZolaPOM/UnitTesting/page_objects/webdriver_wrapper.py
+++ ZolaPOM/UnitTesting/page_objects/webdriver_wrapper.py
@@ -8,80 +8,60 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 from selenium.webdriver.support import expected_conditions as EC # available since 2.26.0
 from selenium.webdriver.common.action_chains import ActionChains
+from time import sleep
 import random
 
 
-class webdriver_wrapper():
+## module level variables separate from webdriver_wrapper:
+mode = 'multi'  # change to 'single' to prevent closing and opening after each test
+_drivers = {}  # so webdriver_wrapper can use existing if mode=='single'
 
+class webdriver_wrapper():
     ################################################
-    ##############GLOBAL VARIABLES##################
+    ############# GLOBAL VARIABLES #################
     ################################################
-    
+
     _browsers = (#"chrome",
         "firefox",
         #"safari",
         )
-    #_authUrl = 'https://zola_stage:zola123@zolaqc.com'
-    #_baseUrl = 'https://zolaqc.com'
-    
-    #r2 = str( random.randint(0,1000000) )
-    
-    ################################################
-    #####################SETUP######################
-    ################################################
-    
-    def open_chrome(self):
-        self._driver = webdriver.Chrome('/Library/Python/2.7/site-packages/chromedriver')
-        self._driver.implicitly_wait(10)
-        self.wait = WebDriverWait(self._driver, 10)
-        
-    def open_firefox(self):
-        self._driver = webdriver.Firefox()
-        self._driver.implicitly_wait(10)
-        self.wait = WebDriverWait(self._driver, 10)
-        
-    def open_safari(self):
-        self._driver = webdriver.Remote(desired_capabilities={'browserName':'safari'})
-        self._driver.implicitly_wait(10)
-        self.wait = WebDriverWait(self._driver, 10)
-        
-    def open_ie(self):
-        self._driver = webdriver.Ie()
-        self._driver.implicitly_wait(10)
-        self.wait = WebDriverWait(self._driver, 10)
-        
-    def __init__(self, arg):
-        
-       
-       # self._authURL = 'https://zolabooks.com'
-       # self._baseURL = 'https://zolabooks.com'
-       # self._authURL = 'https://zola_stage:zola123@peoplefind.divergence.zolaqc.com'
-       # self._baseURL = 'https://peoplefind.divergence.zolaqc.com'
-        self._authURL = 'https://zola_stage:zola123@zolaqc.com' 
-        self._baseURL = 'https://zolaqc.com'
-       # self._authURL = 'https://zola_stage:zola123@luke.divergence.zolaqc.com'                                       
-       # self._baseURL = 'https://luke.divergence.zolaqc.com'        
-       # self._authURL = 'https://zola_stage:zola123@amac.divergence.zolaqc.com'
-       # self._baseURL = 'https://amac.divergence.zolaqc.com'
-       # self._authURL = 'https://zola_stage:zola123@amanda.divergence.zolaqc.com'
-       # self._baseURL = 'https://amanda.divergence.zolaqc.com'
-       # self._authURL = 'https://zola_stage:zola123@wuyou.divergence.zolaqc.com'
-       # self._baseURL = 'https://wuyou.divergence.zolaqc.com'
 
+    _baseURL = 'https://zolaqc.com'
+    #_baseURL = 'https://zolabooks.com'
+    #_baseURL = 'https://peoplefind.divergence.zolaqc.com'
+    #_baseURL = 'https://luke.divergence.zolaqc.com'
+    #_baseURL = 'https://amac.divergence.zolaqc.com'
+    #_baseURL = 'https://amanda.divergence.zolaqc.com'
+    #_baseURL = 'https://wuyou.divergence.zolaqc.com'
+
+    logout_path = '/?ACT=12'
+
+    ################################################
+    #################### SETUP #####################
+    ################################################
+
+    def __init__(self, browser):
+        global _drivers, mode
         self.rand_username_int = str( random.randint(0,1000000) )
-        if (arg == "chrome"):
-            self.open_chrome()
-        elif (arg == "safari"):
-            self.open_safari()
-        elif (arg == "firefox"):
-            self.open_firefox()
+        if mode == 'single' and browser in _drivers:
+            self._driver = _drivers[browser]
+        elif browser == 'chrome':
+            self._driver = webdriver.Chrome('/Library/Python/2.7/site-packages/chromedriver')
+        elif browser == 'safari':
+            self._driver = webdriver.Remote(desired_capabilities={'browserName':'safari'})
+        elif browser == 'firefox':
+            self._driver = webdriver.Firefox()
+        elif browser == 'ie':
+            self._driver = webdriver.Ie()
         else:
-            pass
-        self._driver.get(self._authURL)
+            raise Exception('bad "browser" value passed in to '+self.__class__.__name__)
+        _drivers[browser] = self._driver
         self._driver.implicitly_wait(40)
-       
-    def go_to_zola(self):
-        self._driver.get(self._authURL)
+        self.wait = WebDriverWait(self._driver, 10)
+
+        import re
+        authURL = re.sub(r'(https?://)(.*)', r'\1zola_stage:zola123@\2', self._baseURL)
+        self._driver.get(authURL)
     
     def open_page(self, path):
         self._driver.get(self._baseURL + path)
@@ -93,7 +73,13 @@ class webdriver_wrapper():
             raise AssertionError("Not on correct page.")
         
     def close_the_browser(self):
-        self._driver.quit()
+        global mode
+        self._driver.get(self._baseURL + self.logout_path)
+        if mode == 'single':
+            ## Just sleep to make sure the logout finishes, and don't close browser
+            sleep(0.5)
+        else:
+            self._driver.quit()
         
     def switch_window(self):
         windows = self._driver.window_handles
